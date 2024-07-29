55c4b8372fc0c068257aff30e0b2f9979b5e0dec
Vatsal Khanna <vatsalkhanna1992@gmail.com>
2024-07-29 16:03:02 +0530

    User card class.

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,11 +1,14 @@
 import "./user.css";
 import user from "./user.json";
 import UserCard from './components/UserCard';
+import UserCardClass from "./components/UserCardClass";
 
 function App() {
   return (
     <div className="App">
       <UserCard name={user.name} age={user.age} phoneNumber={user.phoneNumber} address={user.address} />
+      <br />
+      <UserCardClass name={user.name} age={user.age} phoneNumber={user.phoneNumber} address={user.address} />
     </div>
   );
 }
